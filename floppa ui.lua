50e9ac053e320042bcb26011582328cb10ff6004
poopyhead121 <91475324+poopyhead121@users.noreply.github.com>
2022-03-22 15:02:52 -0500

    Update floppa ui.lua

--- floppa ui.lua
+++ floppa ui.lua
@@ -1,7 +1,7 @@
 -- Loader
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 local UI = Library.Load({
-    Title = "Floppa Hub",
+    Title = "Flipper Flopper",
     Style = 4,
     SizeX = 400,
     SizeY = 400,
@@ -9,7 +9,7 @@ local UI = Library.Load({
 })
 -- Pages
 local Floppa = UI.New({
-    Title = "Floppas"
+    Title = "The famous flopper"
 })
 local NonFloppa = UI.New({
     Title = "FE Converted Scripts"
@@ -17,11 +17,41 @@ local NonFloppa = UI.New({
 })
 local Credits = UI.New({
     Title = "Credits"
+        
+})
+local Fling = UI.New({
+    Title = "Fe fling flong 9000"
+    
 })
 
 NonFloppa.Button({
     Text = "FE Shortsword Converted",
     Callback = function()
-       print("Clicked!") 
+       loadstring(game:HttpGet(('https://raw.githubusercontent.com/poopyhead121/floppa-source/main/shortsword.lua'),true))()
+ 
     end
 })
+
+Credits.Button({
+        Text = "Click and press F9 for credits",
+        Callback = function()
+            print('Oofhead, Perception, Emper')
+            end
+        })
+    local Victim = victim
+    
+    Fling.TextField({
+        Text = "Name",
+        Callback = function(Victim)
+        local P=game:GetService'Players'.LocalPlayer
+local C=P.Character or P.CharacterAdded:Wait()
+game:GetService'RunService'.Heartbeat:Connect(function()
+    C:WaitForChild'HumanoidRootPart'.CFrame=CFrame.new(game:GetService'Players':FindFirstChild(Victim).Character:WaitForChild'Head'.Position) * CFrame.new(0,-3.7,0) * CFrame.Angles(9e9,0,0) --loop this
+    
+
+    --C:WaitForChild'HumanoidRootPart'.CFrame=CFrame.new(0,50,0)
+
+ 
+end)
+end
+    })
